b12b3bc9f3f0d1f8477d5f425549e4df63dae925
unknown <mounaadagatla@gmail.com>
2021-10-27 15:54:13 +0530

    Add : stored procedure for EmpPayroll services

--- EmployeePayrole.sql
+++ EmployeePayrole.sql
@@ -49,3 +49,40 @@ select MIN(salary) from Employee_payroll
 WHERE gender = 'F' GROUP BY gender;
 
 select * from Employee_payroll
+
+--- UC8 add department
+alter table Employee_payroll add 
+Phn varchar(20),
+department varchar(20) not null default 'QA',
+Address varchar(20) not null default 'Hyd'
+
+select * from Employee_payroll
+
+---UC9 to have Basic Pay,Deductions, Taxable Pay,Income Tax, Net Pay
+Alter table Employee_payroll ADD Basic_pay money,Deductions money,
+Taxable_pay money,Incometax_pay money ,Netpay money;
+
+----UC10
+insert into Employee_payroll(Name,Salary,Start_date,gender,Phn,department,Basic_pay,Deductions,
+Taxable_pay,Incometax_pay,Netpay) 
+values ('Terissa',25000.00,SYSDATETIME(),'F','236336','CS',2000,1000,1000,1000,20000)
+
+insert into Employee_payroll(Name,Salary,Start_date,gender,Phn,department,Basic_pay,Deductions,
+Taxable_pay,Incometax_pay,Netpay) 
+values ('Terissa',40000.00,SYSDATETIME(),'F','236446','HR',4000,2000,2000,2000,30000)
+
+select * from Employee_payroll
+select * from Employee_payroll where Name='Terissa';
+--ER daigram added for UC10
+
+---UC11 for adding new department
+Create Table Employee_department(
+dept_id int identity(1,1) primary key,
+
+department varchar(20) not null default 'CS'
+);
+
+
+Alter table Employee_payroll ADD dept_id int foreign key references Employee_department(dept_id); 
+update Employee_payroll set dept_id=2 where id=8;
+update Employee_department set department='HR' where dept_id=2;
